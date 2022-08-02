01811806835ce04541852b14f061d626eaf64bcf
kochegaar <106970574+Kochegaar@users.noreply.github.com>
2022-08-02 15:55:42 +0300

    rename

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         Post post = new Post();
-        post.birthday = new Post.FormDate();
         post.name = "Иван";
         post.passport = "4444 № 444444";
         post.patronymic = "Иванович";
         post.phone = "+7(999)-999-99-99";
         post.surname = "Иванович";
         post.subscription = String.valueOf(true);
+
+
+        post.birthday = new Post.FormDate();
         post.birthday.day = 13;
         post.birthday.month = 6;
         post.birthday.year = 1999;
