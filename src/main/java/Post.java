5b092aa0d621de0835221bb6aac1ce2f9c29a32b
kochegaar <106970574+Kochegaar@users.noreply.github.com>
2022-08-02 15:26:47 +0300

    modified input

---
+++ src/main/java/Post.java
@@ -0,0 +1,27 @@
+public class Post {
+    public String name; // поле для имени
+    // добавьте другие поля
+
+    public String passport; // поле для имени
+    // добавьте другие поля
+
+    public String patronymic; // поле для имени
+    // добавьте другие поля
+
+    public String phone; // поле для имени
+    // добавьте другие поля
+
+    public String surname; // поле для имени
+    // добавьте другие поля
+
+    public String subscription;
+
+    public FormDate birthday;
+
+    public static class FormDate {
+        public int day;
+        public int month;
+        public int year;
+    }
+
+}
